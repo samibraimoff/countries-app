21e7ec6815979f55f6783b36195d303d2f344606
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-23 01:24:11 -0500

    header style update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 
 import { Link } from 'react-router-dom';
+import { RiGlobeLine } from 'react-icons/ri';
+
+import '../styles/header.scss';
 
 const Header = () => {
   return (
     <Fragment>
       <nav>
+        <div className='logo-container'>
+          <RiGlobeLine className='logo' />
+          <span>Rest Countries</span>
+        </div>
         <ul>
           <li>
             <Link to='/'>Home</Link>
