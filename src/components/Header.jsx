9442ef8395721eacbff7992cd3130363b285c3a8
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-03-14 23:49:16 -0400

    header style update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,26 @@ const HeaderElement = styled.header`
   box-shadow: var(--shadow);
   background-color: var(--colors-ui-base);
 `;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 const Title = styled.a.attrs({
   href: '/',
-})``;
-const ModeSwitcher = styled.div``;
+})`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
+const ModeSwitcher = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  text-transform: capitalize;
+`;
 
 export const Header = () => {
   const [theme, setTheme] = useState('light');
@@ -32,7 +47,12 @@ export const Header = () => {
         <Wrapper>
           <Title>World countries!</Title>
           <ModeSwitcher onClick={toggleTheme}>
-            <IoMoon /> Light Theme
+            {theme === 'light' ? (
+              <IoMoonOutline size='14px' />
+            ) : (
+              <IoMoon size='14px' />
+            )}
+            <span style={{ marginLeft: '0.75rem' }}>{theme} theme</span>
           </ModeSwitcher>
         </Wrapper>
       </Container>
